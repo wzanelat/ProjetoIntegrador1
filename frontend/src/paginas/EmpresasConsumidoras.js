527955f38580ee9015ddedbf913ab799416e0e8b
WellingtonZanelatto <wellingtonzanelatto@gmail.com>
2024-05-06 23:18:46 -0300

    commit 060520242318

--- frontend/src/paginas/EmpresasConsumidoras.js
+++ frontend/src/paginas/EmpresasConsumidoras.js
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from 'styled-components'
+import CardsConsumidoras from "../Componentes/CardsConsumidoras";
+
+const ListaCards= styled.div`
+    display: grid;
+    grid-template-columns: 200px repeat(auto-fill, 200px);
+    grid-column-gap: 3rem;
+    grid-row-gap: 3rem;
+`;
+
+const ListaEmpresasConsumidoras = styled.div`
+        min-height: 30vh;
+        overflow: hidden;
+        padding: 5rem 10rem;
+`;
 
 const EmpresasConsumidoras = ()=>{
     return(
-        <div>
-            <h1>Empresas Consumidoras</h1>
-        </div>
+        <ListaEmpresasConsumidoras>
+            <ListaCards>
+                <CardsConsumidoras/>
+            </ListaCards>
+        </ListaEmpresasConsumidoras>
     )
 }
 
--- frontend/src/paginas/EmpresasFornecedoras.js
+++ frontend/src/paginas/EmpresasFornecedoras.js
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from 'styled-components'
+import CardsFornecedoras from "../Componentes/CardsFornecedoras";
+
+const ListaCards= styled.div`
+    display: grid;
+    grid-template-columns: 200px repeat(auto-fill, 200px);
+    grid-column-gap: 3rem;
+    grid-row-gap: 3rem;
+`;
+
+const ListaEmpresasFornecedoras = styled.div`
+        min-height: 30vh;
+        overflow: hidden;
+        padding: 5rem 10rem;
+`;
 
 const EmpresasFornecedoras = ()=>{
     return(
-        <div>
-            <h1>Empresas Fornecedoras</h1>
-        </div>
+        <ListaEmpresasFornecedoras>
+            <ListaCards>
+                <CardsFornecedoras/>
+            </ListaCards>
+        </ListaEmpresasFornecedoras>
     )
 }
 
